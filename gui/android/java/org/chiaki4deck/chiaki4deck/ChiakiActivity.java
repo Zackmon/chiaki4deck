81d798806300561657fe7450a31e096752511bf7
zackmon <salemthunder.fist@gmail.com>
2024-06-29 22:31:05 +0300

    Added SDL java code

---
+++ gui/android/java/org/chiaki4deck/chiaki4deck/ChiakiActivity.java
@@ -0,0 +1,37 @@
+package org.chiaki4deck.chiaki4deck;
+
+import org.libsdl.app.SDLActivity;
+import org.libsdl.app.SDL;
+import org.libsdl.app.SDLAudioManager;
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import org.qtproject.qt.android.QtActivityBase;
+
+public class ChiakiActivity extends QtActivityBase{
+    @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            Log.d("myTag", "This is chiaki4deck Activity");
+            super.onCreate(savedInstanceState);
+            //Intent intent = new Intent(this, SDLActivity.class);
+            //startActivity(intent);
+            SDL.initialize();
+            SDL.setContext( this );
+            SDLAudioManager.initialize();
+            SDLAudioManager.setContext( this );
+
+        }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("MySDLActivity", "onDestroy");
+        if (SDL.getContext() == this)
+                        {
+                                SDL.setContext( null );
+                        }
+    }
+
+}
